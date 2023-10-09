498630fc5aec8fe7fe5a990f95a3bf07092f7934
Desi205 <desiwlnwulan@gmail.com>
2023-10-09 20:32:14 +0700

    pertemuan-3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller {
+public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo " Nama saya Desi Wulandari <br>
+    saya tinggal di daerah bantul <br>
+    olahraga yang saya sukai adalah batminton";
+
+}
+}

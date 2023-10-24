62ed6347456e3a4655fa5c70e27eeac74c18b756
Desi205 <desiwlnwulan@gmail.com>
2023-10-24 18:52:02 +0700

    push pemesanan_tiket

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -6,11 +6,30 @@ class Matakuliah extends CI_Controller{
     }
   public function cetak()
     {
-$data = [
+      $this->form_validation->set_rules('kode', 'kode Matakuliah', 'trim|required|numeric|min_length[3]',
+      array('required' => '%s Wajib diisi.',
+            'min_length' => '%s 3 Karakter.',
+            'numeric' => '%s Wajib Diisi Angka.',));
+      $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required',
+      array('required' => '%s Wajib diisi.',));
+      $this->form_validation->set_rules('sks', 'SKS', 'required',
+      array('required' => '%s Wajib diisi.',));
+      
+      if ($this->form_validation->run() == FALSE)
+      {
+              $this->load->view('view-form-matakuliah');
+      }
+      else
+      {
+        $data = [
+
           'kode' => $this->input->post('kode'),
           'nama' => $this->input->post('nama'),
           'sks' => $this->input->post('sks')
                   ];
   $this->load->view('view-data-matakuliah', $data);
+      }
+
+
 }
 }
